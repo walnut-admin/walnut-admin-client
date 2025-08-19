5aa304ae4e656abd5edbf2006b31a89e3415bff8
Zhaocl1997 <674351624@qq.com>
2025-08-19 15:33:10 +0800

    refactor: each refresh would update device

--- src/App/src/scripts/device.ts
+++ src/App/src/scripts/device.ts
@@ -1,10 +1,5 @@
 import { initialDeviceAPI } from '@/api/system/device'
 
-const appFingerprint = useAppStoreFingerprint()
-
 export async function setupDevice() {
-  if (appFingerprint.getFingerprint) {
-    return
-  }
   await initialDeviceAPI()
 }
