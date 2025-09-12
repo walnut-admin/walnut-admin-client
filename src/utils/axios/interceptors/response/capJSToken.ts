513f9fed97a85e1e72a2f2e2f2fca734f2ee6939
Zhaocl1997 <674351624@qq.com>
2025-09-12 13:44:29 +0800

    fix: beforeInit error

--- src/utils/axios/interceptors/response/capJSToken.ts
+++ src/utils/axios/interceptors/response/capJSToken.ts
@@ -1,9 +1,10 @@
 import { SingletonPromise } from '@/utils/queue'
 
-const compStoreCapJS = useStoreCompCapJS()
 const capJSQueue = new SingletonPromise<string>()
 
 export function SingletonPromiseCapJSToken() {
+  const compStoreCapJS = useStoreCompCapJS()
+
   return capJSQueue.run(async () => {
     return await compStoreCapJS.refreshCapJSToken()
   })
