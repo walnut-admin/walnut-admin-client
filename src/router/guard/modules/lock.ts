fdc80ee8bd55106cd5a5ce1ae43e4a6e51a2d551
Zhaocl1997 <674351624@qq.com>
2025-09-15 15:04:49 +0800

    fix: missing add route in lock beforeEach

--- src/router/guard/modules/lock.ts
+++ src/router/guard/modules/lock.ts
@@ -1,4 +1,5 @@
 import type { Router } from 'vue-router'
+import { AppLockRoute } from '@/router/routes/builtin'
 
 export function createLockGuard(router: Router) {
   const appStoreLock = useAppStoreLock()
@@ -6,9 +7,15 @@ export function createLockGuard(router: Router) {
 
   // beforeEach
   router.beforeEach((to) => {
-    if (AppRouter.hasRoute(AppLockName) && appStoreLock.getLockStatus && appStoreLock.getLockRoute) {
-      if (to.name !== AppLockName)
+    if (appStoreLock.getLockStatus && appStoreLock.getLockRoute) {
+      if (AppRouter.hasRoute(AppLockName)) {
+        if (to.name !== AppLockName)
+          return { name: AppLockName }
+      }
+      else {
+        router.addRoute(AppLockRoute)
         return { name: AppLockName }
+      }
     }
 
     if (AppRouter.hasRoute(AppLockName) && !appStoreLock.getLockStatus && to.name === AppLockName) {
