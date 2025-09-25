839ae1dbffc654b376c1dce472293199c85fe7a6
Zhaocl1997 <674351624@qq.com>
2025-09-25 15:07:51 +0800

    refactor: socket credentials & headers support

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -2,91 +2,74 @@ import type { Recordable } from 'easy-fns-ts'
 import type { Socket } from 'socket.io-client'
 import { io } from 'socket.io-client'
 
-const appStoreFingerprint = useAppStoreFingerprint()
+const { ws } = useAppEnvProxy()
+const useProxy = +ws[0] === 1
+const url = useProxy ? `/${ws[4]}` : `${ws[2]}/${ws[4]}`
+const path = useProxy ? ws[1] : ws[3]
 
-/**
- * @description socket
- */
-class SocketService {
-  private static instance: SocketService
-  private _socket: Socket | null = null
+let socket: Socket | null = null
 
-  private constructor() {}
-
-  public static getInstance(): SocketService {
-    if (!SocketService.instance) {
-      SocketService.instance = new SocketService()
-    }
-    return SocketService.instance
+export function getSocket(): Socket {
+  if (!socket) {
+    throw new Error('Socket not initialized. Call initSocket() first.')
   }
-
-  public init(): void {
-    if (this._socket) {
-      console.info('Socket', 'Already initialized')
-      return
-    }
-
-    const { ws } = useAppEnvProxy()
-    const userStoreAuth = useAppStoreUserAuth()
-    const useProxy = +ws[0] === 1
-    const url = useProxy ? `/${ws[4]}` : `${ws[2]}/${ws[4]}`
-    const path = useProxy ? ws[1] : ws[3]
-
-    this._socket = io(url, {
-      path,
-      query: { fingerprint: appStoreFingerprint.getFingerprint, accessToken: userStoreAuth.accessToken },
-    })
-
-    this._socket.on('connect', () => {
-      // turbo-console-disable-next-line
-      console.info('Socket', 'Socket connected.')
-    })
-
-    this._socket.on('error', (e) => {
-      console.error('Socket', e)
-    })
-
-    this._socket.on(AppSocketEvents.FORCE_QUIT(), async (payload: { visitor: string, strategy: string }) => {
-      const ForceQuitMap: Recordable = {
-        FORCE_IMMEDIATE_SIGNOUT: async () => {
-          const userStoreAuth = useAppStoreUserAuth()
-          await userStoreAuth.Signout()
-        },
-        FORCE_COUNTDOWN_MODAL: () => {
-          const compStoreForceQuit = useStoreCompForceQuit()
-          compStoreForceQuit.onOpenForceQuitModal()
-        },
-        MANUAL_COUNTDOWN_MODAL: () => {
-          const compStoreForceQuit = useStoreCompForceQuit()
-          compStoreForceQuit.onOpenForceQuitModal(true)
-        },
-      }
-
-      ForceQuitMap[payload.strategy]()
-    })
-
-    // turbo-console-disable-next-line
-    console.info('Socket', 'Socket initialized successfully')
-  }
-
-  public get socket(): Socket {
-    if (!this._socket) {
-      throw new Error('Socket not initialized. Call init() first.')
-    }
-    return this._socket
-  }
-
-  public destroy(): void {
-    if (this._socket) {
-      this._socket.disconnect()
-      this._socket = null
-      console.info('Socket', 'Socket disconnected.')
-    }
-  }
+  return socket
 }
 
-export const socketService = SocketService.getInstance()
-
 export function setupSocket() {
-  socketService.init()
+  if (socket) {
+    console.info('[Socket] Already initialized')
+    return socket
+  }
+
+  const userStoreAuth = useAppStoreUserAuth()
+  const appStoreLocale = useAppStoreLocale()
+  const appStoreFingerprint = useAppStoreFingerprint()
+
+  socket = io(url, {
+    path,
+    withCredentials: true,
+    extraHeaders: {
+      'Authorization': `Bearer ${userStoreAuth.accessToken}`,
+      'x-language': appStoreLocale.getLocale,
+      'x-fingerprint': appStoreFingerprint.getFingerprint,
+    },
+  })
+
+  socket.on('connect', () => {
+    console.info('[Socket] Connected')
+  })
+
+  socket.on('error', (err) => {
+    console.error('[Socket] Error:', err)
+  })
+
+  socket.on(AppSocketEvents.FORCE_QUIT(), async (payload: { visitor: string, strategy: string }) => {
+    const strategyMap: Recordable = {
+      FORCE_IMMEDIATE_SIGNOUT: async () => {
+        await useAppStoreUserAuth().Signout()
+      },
+      FORCE_COUNTDOWN_MODAL: () => {
+        useStoreCompForceQuit().onOpenForceQuitModal()
+      },
+      MANUAL_COUNTDOWN_MODAL: () => {
+        useStoreCompForceQuit().onOpenForceQuitModal(true)
+      },
+    }
+
+    const handler = strategyMap[payload.strategy]
+    if (handler)
+      await handler()
+  })
+
+  console.info('[Socket] Initialized successfully')
+  return socket
+}
+
+export function destroySocket() {
+  if (socket) {
+    socket.disconnect()
+    socket = null
+    console.info('[Socket] Disconnected')
+  }
 }
