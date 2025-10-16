a4e1c9f1a78f5bdab0c56e3ef1ad15e6301c0154
Zhaocl1997 <674351624@qq.com>
2025-10-16 12:42:03 +0800

    refactor: table right icon style

--- src/components/UI/Table/src/components/header/right/index.vue
+++ src/components/UI/Table/src/components/header/right/index.vue
@@ -13,15 +13,13 @@ const { getProps: tableProps } = tablePropsCtx
 
 <template>
   <div class="hstack items-center gap-x-2 children:cursor-pointer">
-    <div>
-      <WIconButton
-        v-if="!tableProps.polling"
-        :icon-props="{ icon: 'ant-design:reload-outlined' }"
-        :button-props="{ disabled: tableProps.loading, onClick: onApiList }"
-        tooltip
-        :tooltip-msg="$t('app.base.refresh')"
-      />
-    </div>
+    <WIconButton
+      v-if="!tableProps.polling"
+      :icon-props="{ icon: 'ant-design:reload-outlined' }"
+      :button-props="{ disabled: tableProps.loading, onClick: onApiList }"
+      tooltip
+      :tooltip-msg="$t('app.base.refresh')"
+    />
 
     <!-- @vue-generic {T} -->
     <TableHeaderRightPolling v-if="tableProps.polling && tableProps.polling > 5000" />
