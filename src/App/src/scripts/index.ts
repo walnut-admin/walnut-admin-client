9daaf0b9d8e2b3fb836b523f370e8f6303736371
Zhaocl1997 <674351624@qq.com>
2025-11-06 16:09:08 +0800

    fix: device cache & fingerprint ensure

--- src/App/src/scripts/index.ts
+++ src/App/src/scripts/index.ts
@@ -6,6 +6,10 @@ export async function setupAppScripts() {
   const appStoreFingerprint = useAppStoreFingerprint()
   await appStoreFingerprint.setupFingerprint()
 
+  // send beacon with left: false ensure device cache set
+  // also to make sure the fingerprint is sent
+  sendUserMonitorBeacon({ left: false })
+
   const appStoreGeoIP = useAppStoreGeoIP()
   await appStoreGeoIP.setupGeoIP()
   await appStoreGeoIP.setupDeviceId()
