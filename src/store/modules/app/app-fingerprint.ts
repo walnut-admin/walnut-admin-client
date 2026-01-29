792700d94994cb338d829b8bf7fd0504b976b226
Zhaocl1997 <674351624@qq.com>
2026-01-29 17:01:23 +0800

    refactor: fingerprint components

--- src/store/modules/app/app-fingerprint.ts
+++ src/store/modules/app/app-fingerprint.ts
@@ -1,58 +1,12 @@
-import type { BuiltinComponents, UnknownComponents } from '@fingerprintjs/fingerprintjs'
 import type { AxiosRequestConfig } from 'axios'
 import type { IStoreApp } from '@/store/types'
 import FingerprintJS from '@fingerprintjs/fingerprintjs'
-import { pick } from 'lodash-es'
 import { defineStore } from 'pinia'
 import { enhancedBase64LocalStorage } from '@/utils/persistent/enhance'
 import { useAppStorageSync } from '@/utils/persistent/storage/sync'
 import { detectDeviceType } from '@/utils/shared'
 import { StoreKeys } from '../../constant'
 import { store } from '../../pinia'
-
-// TODO edge/chrome in same pc has same fingerprint?
-const excludeComponents: (keyof BuiltinComponents)[] = [
-  'applePay',
-  'architecture',
-  'audio',
-  'audioBaseLatency',
-  'canvas',
-  'colorDepth',
-  'colorGamut',
-  'contrast',
-  'cookiesEnabled',
-  'cpuClass',
-  'deviceMemory',
-  'domBlockers',
-  'fontPreferences',
-  'fonts',
-  'forcedColors',
-  'hardwareConcurrency',
-  'hdr',
-  'indexedDB',
-  'invertedColors',
-  'languages',
-  'localStorage',
-  'math',
-  'monochrome',
-  'openDatabase',
-  'osCpu',
-  'pdfViewerEnabled',
-  'platform',
-  'plugins',
-  'privateClickMeasurement',
-  'reducedMotion',
-  'reducedTransparency',
-  'screenFrame',
-  'screenResolution',
-  'sessionStorage',
-  'timezone',
-  'touchSupport',
-  'vendor',
-  'vendorFlavors',
-  'webGlBasics',
-  'webGlExtensions',
-]
 
 const useAppStoreFingerprintInside = defineStore(StoreKeys.APP_FINGERPRINT, {
   state: (): IStoreApp.Fingerprint => ({
@@ -72,19 +26,10 @@ const useAppStoreFingerprintInside = defineStore(StoreKeys.APP_FINGERPRINT, {
     async setupFingerprint() {
       if (this.getFingerprint)
         return
-
       const fpPromise = FingerprintJS.load({})
-
-      // Get the visitor identifier when you need it.
       const fp = await fpPromise
       const result = await fp.get()
-
-      // you can add your custom components here
-      const components: UnknownComponents = pick(result.components, excludeComponents)
-
-      const visiterId = FingerprintJS.hashComponents(components)
-
-      this.fingerprint = visiterId
+      this.fingerprint = result.visitorId
     },
 
     axiosReqInterceptorBuildFingerprint(_config: AxiosRequestConfig) {
