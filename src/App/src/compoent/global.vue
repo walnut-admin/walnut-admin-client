574c020754a94adae49a845fac30893da1463dbc
Zhaocl1997 <falling9712@gamil.com>
2026-04-03 17:06:24 +0800

    perf: global modal animation optimize

--- src/App/src/compoent/global.vue
+++ src/App/src/compoent/global.vue
@@ -6,10 +6,31 @@ defineOptions({
 const compStoreReloadPrompt = useStoreCompReloadPrompt()
 const compStoreCapJS = useStoreCompCapJS()
 const compStoreVerifyAuth = useStoreCompVerifyAuth()
+
+const reloadPrompt = useGlobalAsyncComponent(
+  computed(() => compStoreReloadPrompt.getShow),
+  { unmountDelay: 200 },
+)
+
+const cap = useGlobalAsyncComponent(
+  computed(() => compStoreCapJS.getShow),
+  { unmountDelay: 350 },
+)
+
+const verifyAuth = useGlobalAsyncComponent(
+  computed(() => compStoreVerifyAuth.getShow),
+  { unmountDelay: 350 },
+)
 </script>
 
 <template>
-  <WCap v-if="compStoreCapJS.getShow" />
-  <WReloadPrompt v-if="compStoreReloadPrompt.getShow" />
-  <WVerifyAuth v-if="compStoreVerifyAuth.getShow" />
+  <div v-if="cap.mounted" v-show="cap.visible">
+    <WCap />
+  </div>
+  <div v-if="reloadPrompt.mounted" v-show="reloadPrompt.visible">
+    <WReloadPrompt />
+  </div>
+  <div v-if="verifyAuth.mounted" v-show="verifyAuth.visible">
+    <WVerifyAuth />
+  </div>
 </template>
