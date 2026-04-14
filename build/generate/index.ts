8ca36e7e4dc59771504e8e1d38fd7f84af77de26
Zhaocl1997 <falling9712@gamil.com>
2026-04-14 15:30:32 +0800

    chore: remove useless build code

--- build/generate/index.ts
+++
@@ -1,7 +0,0 @@
-import { BuildUtilsExec } from '../utils'
-;
-
-(async function () {
-  await BuildUtilsExec('esno build/generate/genPaths.ts')
-  await BuildUtilsExec('esno build/generate/genJSONSchemas.ts')
-})()
